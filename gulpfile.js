a252dfd3266b6ca884ef59004050218382ecf1b5
Boston Dell-Vandenberg <boston@pomelodesign.com>
2015-01-08 14:53:15 -0500

    convert to SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,26 @@ var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var prefix = require('gulp-autoprefixer');
+var minify = require('gulp-minify-css');
 
-gulp.task('default', function(){
+gulp.task('styles', function () {
+  return gulp.src('./angular-socialshare.scss')
+    .pipe(sass({
+      precision: 10
+    }))
+    .pipe(prefix())
+    .pipe(minify({ keepSpecialComments: 1 }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('scripts', function (){
   gulp.src('./angular-socialshare.js')
   .pipe(rename('angular-socialshare.min.js'))
   .pipe(uglify())
   .pipe(gulp.dest('./'));
 });
+
+gulp.task('default', ['styles', 'scripts']);
